f1973cd87db45c37793822a2618c9a2cda908ffa
massiferrouk <massiferrouk3@gmail.com>
2025-03-06 16:39:20 +0100

    avancement dans authentification

---
+++ src/main/java/com/massi/config/JwtConstant.java
@@ -0,0 +1,6 @@
+package com.massi.config;
+
+public class JwtConstant {
+    public static final String SECRET_KEY = "gfsdfsjdfbjhvbsdjvhbdjfbrgjrgrhgergzegyzftzeatzrayegurkzrugzf";
+    public static final String JWT_HEADER = "Authorization ";
+}
